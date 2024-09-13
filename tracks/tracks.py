cb6542621bbef1b3ed14e0fa32b0b95df82e8bd6
oleksandra-o <156704678+oleksandra-o@users.noreply.github.com>
2024-09-13 13:51:28 -0700

    Update tracks.py

--- tracks/tracks.py
+++ tracks/tracks.py
@@ -42,16 +42,18 @@ handle = open('tracks.csv')
 for line in handle:
     line = line.strip();
     pieces = line.split(',')
-    if len(pieces) < 6 : continue
+    if len(pieces) < 7 : continue
 
     name = pieces[0]
     artist = pieces[1]
     album = pieces[2]
-    count = pieces[3]
-    rating = pieces[4]
-    length = pieces[5]
+    genre = pieces[3]  # Added genre
+    count = pieces[4]
+    rating = pieces[5]
+    length = pieces[6]
 
-    print(name, artist, album, count, rating, length)
+
+    print(name, artist, album, genre, count, rating, length)
 
     cur.execute('''INSERT OR IGNORE INTO Artist (name) 
         VALUES ( ? )''', ( artist, ) )
@@ -67,5 +69,14 @@ for line in handle:
         (title, album_id, len, rating, count) 
         VALUES ( ?, ?, ?, ?, ? )''', 
         ( name, album_id, length, rating, count ) )
+    
+    # Insert genre
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
+        VALUES ( ? )''', (genre,))
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre,))
+    genre_id = cur.fetchone()[0]
+
+   
+
 
     conn.commit()
